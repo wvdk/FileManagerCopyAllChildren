38724970773abf225d567f2d64059587c466b73b
Wes Van der Klomp <wes.vdk@icloud.com>
2020-11-18 16:20:47 -0800

    add platforms

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "FileManagerCopyAllChildren",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v12)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
